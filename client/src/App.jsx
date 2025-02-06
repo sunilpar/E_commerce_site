5132575981bf16bba9675cd89b85b7897046c574
sunilpar <sunilformal42@gmail.com>
2025-02-06 10:14:43 +0545

    rollout error debug in vercel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from "./backend/auth"
-import {login} from "./store/authSlice"
+import {login} from "./store/authslice"
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import { Outlet } from 'react-router-dom'
